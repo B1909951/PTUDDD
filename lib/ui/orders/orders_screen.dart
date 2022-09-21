98ebe99dedcb8b2d62acd8f82856d44e8ca0357a
Chanh Nam <hachanhnam10@gmail.com>
2022-09-21 09:51:12 +0700

    Xay dung drawer va dinh tuyen theo ten

--- lib/ui/orders/orders_screen.dart
+++ lib/ui/orders/orders_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../shared/app_drawer.dart';
 import 'order_manager.dart';
 import 'order_item_card.dart';
 
 class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
   const OrdersScreen({super.key});
 
   @override
@@ -16,6 +19,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your Orders'),
       ),
+      drawer: const AppDrawer(),
       body: ListView.builder(
         itemCount: ordersManager.orderCount,
         itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
